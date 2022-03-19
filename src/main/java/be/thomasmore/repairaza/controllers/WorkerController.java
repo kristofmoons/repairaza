834e71695d594c03eb4eb556fca0a79f8900900d
kristofmoons <r0696321@student.thomasmore.be>
2022-03-19 21:09:00 +0100

    css updated en start workerdetails

--- src/main/java/be/thomasmore/repairaza/controllers/WorkerController.java
+++ src/main/java/be/thomasmore/repairaza/controllers/WorkerController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Optional;
 
@@ -22,5 +23,19 @@ public class WorkerController {
         model.addAttribute("workers",allWorkers);
         return "workerlist";
 
+    }
+
+    @GetMapping({"/workerdetails/{id}","/workerdetails"})
+    public String workerdetails(Model model, @PathVariable(required = false) Integer id){
+        if (id == null) return "workerdetails";
+
+        Optional<worker> workerFromDb = workerRepository.findById(id);
+        if (workerFromDb.isPresent()){
+            model.addAttribute("worker",workerFromDb.get());
+        }
+        int nrOfWorkers = (int) workerRepository.count();
+        model.addAttribute("prevId",  id > 1 ? id-1 : nrOfWorkers);
+        model.addAttribute("nextId", id < nrOfWorkers ? id + 1 : 1);
+        return "workerdetails";
     }
 }
