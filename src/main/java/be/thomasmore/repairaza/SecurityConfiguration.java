74a6c55d5f3d0e5857e6b7e28d1858b451490c96
kristofmoons <r0696321@student.thomasmore.be>
2022-03-29 20:42:16 +0200

    wachtwoord kan gedecrypteerd worden

--- src/main/java/be/thomasmore/repairaza/SecurityConfiguration.java
+++ src/main/java/be/thomasmore/repairaza/SecurityConfiguration.java
@@ -1,17 +1,43 @@
 package be.thomasmore.repairaza;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-    @EnableWebSecurity
-    public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-        protected void configure(HttpSecurity http) throws Exception {
-            http.authorizeRequests()
-                    .antMatchers("/admin/**").authenticated()
-                    .anyRequest().permitAll()
-                    .and().formLogin();
-            http.csrf().ignoringAntMatchers("/h2-console/**")
-                    .and().headers().frameOptions().sameOrigin();
-        }
+import javax.sql.DataSource;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private DataSource dataSource;
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin/**").authenticated()
+                .anyRequest().permitAll()
+                .and().formLogin();
+        http.csrf().ignoringAntMatchers("/h2-console/**")
+                .and().headers().frameOptions().sameOrigin();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery(
+                        "select username,password,true from user where username = ?")
+                .authoritiesByUsernameQuery(
+                        "select username, role from user where username = ?");
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
