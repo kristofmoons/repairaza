884db3dec0c6080683a1737ae863512c930363f6
kristofmoons <r0696321@student.thomasmore.be>
2022-03-29 20:31:22 +0200

    login button

--- src/main/java/be/thomasmore/repairaza/controllers/HomeController.java
+++ src/main/java/be/thomasmore/repairaza/controllers/HomeController.java
@@ -1,13 +1,23 @@
 package be.thomasmore.repairaza.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+
+import java.security.Principal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller
 public class HomeController {
+    private Logger logger = LoggerFactory.getLogger(HomeController.class);
 
     @GetMapping({"/","home","/index"})
-    public String home(){
+    public String home(Model model, Principal principal){
+        final String loginName = (principal != null)? principal.getName():"Nobody";
+        logger.info("home - logged in: "+loginName);
         return "home";
     }
 
