6509722b81f81140653616bbb32726fb8df73fde
kristofmoons <r0696321@student.thomasmore.be>
2022-03-29 20:15:57 +0200

    security geconfigd

---
+++ src/main/java/be/thomasmore/repairaza/SecurityConfiguration.java
@@ -0,0 +1,15 @@
+package be.thomasmore.repairaza;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+    @EnableWebSecurity
+    public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests()
+                    .antMatchers("/admin/**").authenticated()
+                    .anyRequest().permitAll()
+                    .and().formLogin();
+        }
+}
