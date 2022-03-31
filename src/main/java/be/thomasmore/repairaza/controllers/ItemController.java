abac810e8904871ccd0684d81e14ba73a056d830
kristofmoons <r0696321@student.thomasmore.be>
2022-03-31 14:46:28 +0200

    liefhebber kan items leuk vinden

--- src/main/java/be/thomasmore/repairaza/controllers/ItemController.java
+++ src/main/java/be/thomasmore/repairaza/controllers/ItemController.java
@@ -1,7 +1,9 @@
 package be.thomasmore.repairaza.controllers;
 
 import be.thomasmore.repairaza.model.Item;
+import be.thomasmore.repairaza.model.Liefhebber;
 import be.thomasmore.repairaza.repositories.ItemRepository;
+import be.thomasmore.repairaza.repositories.LiefhebberRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +11,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.security.Principal;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,20 +23,42 @@ import java.util.Optional;
 public class ItemController {
     @Autowired
     private ItemRepository itemRepository;
+    @Autowired
+    private LiefhebberRepository liefhebberRepository;
 
     private Logger logger =  LoggerFactory.getLogger(ItemController.class);
 
+    private Item findItemById(Collection<Item> items, int itemId) {
+        for (Item i : items) {
+            if (i.getId() == itemId) return i;
+        }
+        return null;
+    }
+
     @GetMapping({"/itemDetails/{id}","/itemDetails"})
-    public String itemDetails(Model model, @PathVariable(required = false) Integer id){
+    public String itemDetails(Model model,
+                              Principal principal,
+                              @PathVariable(required = false) Integer id){
         if (id == null) return "itemDetails";
 
         Optional<Item> itemFromDb = itemRepository.findById(id);
         if (itemFromDb.isPresent()){
+            Item foundItemForLiefhebber = null;
+            if (principal != null) {
+                Optional<Liefhebber> optionalLiefhebber = liefhebberRepository.findByUserUsername(principal.getName());
+                if (optionalLiefhebber.isPresent()) {
+                    Liefhebber liefhebber = optionalLiefhebber.get();
+                    foundItemForLiefhebber = findItemById(liefhebber.getItems(), id);
+                }
+            }
+
             model.addAttribute("item",itemFromDb.get());
             long nrOfItems= itemRepository.count();
             model.addAttribute("prevId",  id > 1 ? id-1 : nrOfItems);
             model.addAttribute("nextId", id < nrOfItems ? id + 1 : 1);
-        }
+            model.addAttribute("vindLeuk", foundItemForLiefhebber != null);
+
+                }
         return "itemDetails";
     }
 
@@ -82,4 +109,27 @@ public class ItemController {
 
         return "itemlist";
     }
+
+    @PostMapping({"/itemleuk", "/itemleuk/{id}"})
+    public String itemLeukToggle(Model model,
+                                   Principal principal,
+                                   @PathVariable(required = false) Integer id) {
+        if (id == null) return "redirect:/itemlist";
+        if (principal == null) return "redirect:/itemDetails/" + id;
+
+        Optional<Liefhebber> optionalLiefhebber = liefhebberRepository.findByUserUsername(principal.getName());
+        Optional<Item> optionalItem = itemRepository.findById(id);
+
+        if (optionalItem.isPresent() && optionalLiefhebber.isPresent()) {
+            Liefhebber liefhebber = optionalLiefhebber.get();
+            Item foundItemforLiefhebber = findItemById(liefhebber.getItems(), id);
+            if (foundItemforLiefhebber == null)
+                liefhebber.getItems().add(optionalItem.get());
+            else
+                liefhebber.getItems().remove(foundItemforLiefhebber);
+            liefhebberRepository.save(liefhebber);
+        }
+        return "redirect:/itemDetails/" + id;
+    }
+
 }
--- src/main/java/be/thomasmore/repairaza/repositories/LiefhebberRepository.java
+++ src/main/java/be/thomasmore/repairaza/repositories/LiefhebberRepository.java
@@ -3,5 +3,8 @@ package be.thomasmore.repairaza.repositories;
 import be.thomasmore.repairaza.model.Liefhebber;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface LiefhebberRepository extends CrudRepository <Liefhebber, Integer> {
+    Optional<Liefhebber> findByUserUsername(String name);
 }
